e6a4cf61de62d58875280a9456ada885433d6e5a
Keebu <olkkuh@hotmail.com>
2021-09-13 21:24:59 +0300

    First commit, 3.1-3.3 done

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express")
+const app = express();
+
+app.use(express.json());
+
+const persons = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find(person => person.id === id);
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end();
+  }
+
+})
+
+
+app.get('/info', (request, response) => {
+
+  response.send(`<h1>Phonebook has info for ${persons.length} people</h1> <p>${new Date()}</p>`)
+})
+
+
+
+
+const PORT = 3001;
+app.listen(PORT);
+console.log(`Server running on port ${PORT}`)
