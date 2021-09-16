4a4f906e7c50c41e85d8fc036cb935f346e36f3a
Keebu <olkkuh@hotmail.com>
2021-09-16 16:44:01 +0300

    uibuild

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors')
 app.use(cors());
 app.use(express.json());
+app.use(express.static('build'))
 const morgan = require('morgan');
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
