2bf19e1126f43f65f58fc939b65c7260d3cfdf05
Oliver Hyryläinen <itzkinkkuu@gmail.com>
2021-09-28 22:38:20 +0300

    added ESLint, 3.22. done

--- index.js
+++ index.js
@@ -1,73 +1,70 @@
 require('dotenv').config();
-const express = require("express")
+const express = require('express');
 const app = express();
 const cors = require('cors');
-const Person = require("./models/person")
+const Person = require('./models/person');
 const morgan = require('morgan');
-const e = require('express');
 app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 morgan.token('body', (request, response) => {
-  return JSON.stringify(request.body)
-})
+  return JSON.stringify(request.body);
+});
 
 //loggaa pyyntöjen tiedot
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-app.use(requestLogger)
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+app.use(requestLogger);
 
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons);
-  })
-})
+  });
+});
 
 app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
     if (person) {
-      response.json(person)
+      response.json(person);
     } else {
-      response.status(404).end()
+      response.status(404).end();
     }
   })
-    .catch(err => next(err))
-})
+    .catch(err => next(err));
+});
 
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
-      response.status(204).end()
+      response.status(204).end();
     })
-    .catch(err => next(err))
-})
-
+    .catch(err => next(err));
+});
 
 app.get('/info', (request, response) => {
 
-  let amount;
   Person.count().then(count => {
-    response.send(`<h1>Phonebook has info for ${count} people</h1> <p>${new Date()}</p>`)
-  })
-})
+    response.send(`<h1>Phonebook has info for ${count} people</h1> <p>${new Date()}</p>`);
+  });
+});
 
 
 //Lähetetään palvelimelle tietoa
 app.post('/api/persons', (request, response, next) => {
-  const body = request.body
+  const body = request.body;
 
   const person = new Person({
     name: body.name,
-    number: body.number || ""
-  })
+    number: body.number || ''
+  });
 
   person
     .save()
@@ -75,17 +72,17 @@ app.post('/api/persons', (request, response, next) => {
       return savedPerson.toJSON();
     })
     .then(savedAndFormattedPerson => {
-      response.json(savedAndFormattedPerson)
+      response.json(savedAndFormattedPerson);
     })
-    .catch(error => next(error))
-})
+    .catch(error => next(error));
+});
 
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body;
   const person = {
     name: body.name,
     number: body.number
-  }
+  };
 
   /*Person.findByIdAndUpdate(request.params.id, person, { new: true })
     .then(updatedPerson => {
@@ -94,37 +91,37 @@ app.put('/api/persons/:id', (request, response, next) => {
     .catch(err => next(err))
   */
 
-  const opts = { runValidators: true }
+  const opts = { runValidators: true };
   Person.findOneAndUpdate({ id: request.params.id }, person, opts, { new: true })
     .then(updatedPerson => {
-      response.json(updatedPerson)
+      response.json(updatedPerson);
     })
-    .catch(err => next(err))
-})
+    .catch(err => next(err));
+});
 
 //olemattomien routejen käsittely
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 app.use(unknownEndpoint);
 
 //käsittelee virheet
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error.message);
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === "ValidationError") {
-    return response.status(400).json({ error: error.message })
+    return response.status(400).send({ error: 'malformatted id' });
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message });
   }
 
-  next(error)
-}
-app.use(errorHandler)
+  next(error);
+};
+app.use(errorHandler);
 
 
 
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
-console.log(`Server running on port ${PORT}`)
+console.log(`Server running on port ${PORT}`);
--- models/person.js
+++ models/person.js
@@ -1,17 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
-const url = process.env.MONGODB_URI
+const url = process.env.MONGODB_URI;
 
-console.log('connecting to', url)
+console.log('connecting to', url);
 
 mongoose.connect(url)
   .then(result => {
-    console.log("connected to MongoDB")
+    console.log('connected to MongoDB');
   })
   .catch(err => {
-    console.log("error connecting to MongoDB: ", err.message)
-  })
-  ;
+    console.log('error connecting to MongoDB: ', err.message);
+  });
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -25,15 +24,15 @@ const personSchema = new mongoose.Schema({
     required: true,
     minlength: 8
   }
-})
+});
 
-personSchema.plugin(uniqueValidator)
+personSchema.plugin(uniqueValidator);
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
   }
-})
+});
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema);
